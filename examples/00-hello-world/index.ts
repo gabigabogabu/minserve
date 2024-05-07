fc94fe964d1c94310370623c9d54db378b9bdff3
gabigabogabu <gaschu95@gmail.com>
2024-05-07 23:20:33 +0200

    fix indents in examples

--- examples/00-hello-world/index.ts
+++ examples/00-hello-world/index.ts
@@ -1,9 +1,9 @@
 import { minserve } from 'minserve';
 
 const server = minserve({
-    requestHandler: async (request) => {
-        return { body: 'Hello world!' };
-    }
+  requestHandler: async (request) => {
+    return { body: 'Hello world!' };
+  }
 });
 
 server.listen(3000, () => { console.log('Server started') });
--- examples/02-not-so-simple-todo/src/modules/todo/todo.controller.ts
+++ examples/02-not-so-simple-todo/src/modules/todo/todo.controller.ts
@@ -4,46 +4,46 @@ import { HttpStatusCode, MinserveRequest, parseJsonBody, parsePathParams } from 
 
 
 const parseAndValidateId = (request: MinserveRequest) => {
-    const pathParams = parsePathParams(request, '/todos/id');
+  const pathParams = parsePathParams(request, '/todos/id');
 
-    const uuidValidator = string().uuid().required();
-    const validatedUUID = uuidValidator.validateSync(pathParams.id[0]);
+  const uuidValidator = string().uuid().required();
+  const validatedUUID = uuidValidator.validateSync(pathParams.id[0]);
 
-    return validatedUUID;
+  return validatedUUID;
 }
 
 const createTodo = async (request: MinserveRequest) => {
-    const body = await parseJsonBody(request);
+  const body = await parseJsonBody(request);
 
-    const todoPostSchema = object({ item: string().required() });
-    const todo = todoPostSchema.validateSync(body);
+  const todoPostSchema = object({ item: string().required() });
+  const todo = todoPostSchema.validateSync(body);
 
-    const newTodo = await TodoService.createTodo(todo);
+  const newTodo = await TodoService.createTodo(todo);
 
-    return { body: JSON.stringify(newTodo), statusCode: HttpStatusCode.CREATED };
+  return { body: JSON.stringify(newTodo), statusCode: HttpStatusCode.CREATED };
 }
 
 const listTodo = async (request: MinserveRequest) => {
-    const todos = await TodoService.listTodos();
-    return { body: JSON.stringify(todos), statusCode: HttpStatusCode.OK };
+  const todos = await TodoService.listTodos();
+  return { body: JSON.stringify(todos), statusCode: HttpStatusCode.OK };
 }
 
 const deleteTodo = async (request: MinserveRequest) => {
-    const uuid = parseAndValidateId(request);
-    await TodoService.deleteTodo(uuid);
-    return { statusCode: 204 };
+  const uuid = parseAndValidateId(request);
+  await TodoService.deleteTodo(uuid);
+  return { statusCode: 204 };
 }
 
 const markAsDone = async (request: MinserveRequest) => {
-    const uuid = parseAndValidateId(request);
-    const todo = await TodoService.markTodoAsDone(uuid);
-    return { body: JSON.stringify(todo), statusCode: HttpStatusCode.OK };
+  const uuid = parseAndValidateId(request);
+  const todo = await TodoService.markTodoAsDone(uuid);
+  return { body: JSON.stringify(todo), statusCode: HttpStatusCode.OK };
 }
 
 const markAsUndone = async (request: MinserveRequest) => {
-    const uuid = parseAndValidateId(request);
-    const todo = await TodoService.markTodoAsUndone(uuid);
-    return { body: JSON.stringify(todo), statusCode: HttpStatusCode.OK };
+  const uuid = parseAndValidateId(request);
+  const todo = await TodoService.markTodoAsUndone(uuid);
+  return { body: JSON.stringify(todo), statusCode: HttpStatusCode.OK };
 }
 
 export { createTodo, listTodo, deleteTodo, markAsDone, markAsUndone };
